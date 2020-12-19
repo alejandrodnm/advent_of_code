a29a0a727b8573631e99283942c84a091c926957
Alejandro Do Nascimento <alejandrodnm@gmail.com>
2020-12-19 10:29:55 +0100

    2020: add d5 and d6

--- y2020-haskell/app/Main.hs
+++ y2020-haskell/app/Main.hs
@@ -1,17 +1,23 @@
 module Main where
 
-import D1 ( d1 )
-import D2 ( d2 )
-import D3 ( d3 )
-import D4 ( d4 )
+import D1 (d1)
+import D2 (d2)
+import D3 (d3)
+import D4 (d4)
+import D5 (d5)
+import D6 (d6)
 
 main :: IO ()
 main = do
-    putStrLn "day 1"
-    d1
-    putStrLn "day 2"
-    d2
-    putStrLn "day 3"
-    d3
-    putStrLn "day 4"
-    d4
+  putStrLn "day 1"
+  d1
+  putStrLn "day 2"
+  d2
+  putStrLn "day 3"
+  d3
+  putStrLn "day 4"
+  d4
+  putStrLn "day 5"
+  d5
+  putStrLn "day 6"
+  d6
